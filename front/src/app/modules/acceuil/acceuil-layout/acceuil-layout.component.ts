574d2c46b9ceb9dd83d3f80c9e8c179bba13d711
Hossameddine-khamma <66746696+Hossameddine-khamma@users.noreply.github.com>
2022-11-30 11:05:28 +0100

    fix httpservice

--- front/src/app/modules/acceuil/acceuil-layout/acceuil-layout.component.ts
+++ front/src/app/modules/acceuil/acceuil-layout/acceuil-layout.component.ts
@@ -2,8 +2,6 @@ import { HttpService } from './../../../shared/services/http.service';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
-import {HttpClient} from '@angular/common/http'
-
 @Component({
   selector: 'app-acceuil_layout',
   templateUrl: './acceuil-layout.component.html',
