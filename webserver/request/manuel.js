4b79dda7c16abc8df1b78a86c76fadcb4d384188
Hossameddine-khamma <66746696+Hossameddine-khamma@users.noreply.github.com>
2023-03-01 16:29:18 +0100

    update manuel front end

--- webserver/request/manuel.js
+++ webserver/request/manuel.js
@@ -1,39 +1,40 @@
-const { exec } = require('child_process');
-const express = require('express');
+const { exec } = require("child_process");
+const express = require("express");
 const router = express.Router();
 
 class Response {
-    constructor(status, error, success) {
-        this.status = status,
-        this.error = error,
-        this.success = success
-    }
+  constructor(status, error, success) {
+    (this.status = status), (this.error = error), (this.success = success);
+  }
 }
 
 class PythonResponse {
-    constructor(status, message) {
-        this.status = status;
-        this.message = message;
-    }
+  constructor(status, message) {
+    this.status = status;
+    this.message = message;
+  }
 }
 
-router.get('/:table_name/:limit/:pageIndex', (req, res) => {
+router.get("/:table_name", (req, res) => {
   var table_name = req.params.table_name;
-  var limit = req.params.limit;
-  var pageIndex = req.params.pageIndex;
-  if (table_name && limit && pageIndex) {
-      let sendResult = {
-          table: "",
-          data: []
-      }
-      sendResult.table = table_name;
-      var offset = limit * pageIndex;
-      let command = 'python scripts/ner/detection.py ' + table_name + " " + process.env.DATABASE;
-      execCommand(res, command);
+  if (table_name) {
+    let sendResult = {
+      table: "",
+      data: [],
+    };
+    sendResult.table = table_name;
+    let command =
+      "python scripts/ner/detection.py " +
+      table_name +
+      " " +
+      process.env.DATABASE;
+    execCommand(res, command);
   } else {
-      res.status(404).send(new Response(404, 'Error 404 : Missing table name or limit', ''));
-  }   
-})
+    res
+      .status(404)
+      .send(new Response(404, "Error 404 : Missing table name or limit", ""));
+  }
+});
 
 function execCommand(res, command) {
   exec(command, (err, stdout, stderr) => {
@@ -45,12 +46,20 @@ function execCommand(res, command) {
     //console.log("err : " + err)
     if (err) {
       responsePython = new PythonResponse(err.code, stdout);
-      res.status(500).send(new Response(500, responsePython.message + " : " + responsePython.status, ''));
+      res
+        .status(500)
+        .send(
+          new Response(
+            500,
+            responsePython.message + " : " + responsePython.status,
+            ""
+          )
+        );
     } else {
-      res.setHeader('content-type', 'application/json');
+      res.setHeader("content-type", "application/json");
       res.status(200).send(json);
     }
-  })
+  });
 }
 
 module.exports = router;
